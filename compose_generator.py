787a490da4727353384778f6c9292daba9408734
AlejoSendra1 <alejo.sendra12@gmail.com>
2026-03-13 23:55:34 -0300

    basic sh file created according to assigment and python script created to generate the wanted yaml file with dinamic clients

---
+++ compose_generator.py
@@ -0,0 +1,86 @@
+import sys
+FILE_NAME_ARGP = 0
+CLIENTS_NUM_ARGP = 1
+DOCKER_PROY_NAME = "name: tp0"
+DOCKER_SERVER_CONFIG = [
+    "  server:\n",
+    "    container_name: server\n",
+    "    image: server:latest\n",
+    "    entrypoint: python3 /main.py\n", 
+    "    environment:\n",
+    "      - PYTHONUNBUFFERED=1\n", 
+    "      - LOGGING_LEVEL=DEBUG\n",
+    "    networks:\n",
+    "      - testing_net\n"
+]
+
+DOCKER_NETWORKS_CONFIG = [
+    "networks:\n", 
+    "  testing_net:\n", 
+    "    ipam:\n", 
+    "      driver: default\n",
+    "      config:\n", 
+    "        - subnet: 172.25.125.0/24\n"
+]
+
+def main():
+    with open(sys.argv[FILE_NAME_ARGP], 'w') as f:
+        f.writelines(DOCKER_PROY_NAME+"\n")
+        f.write("services:"+"\n")
+        f.writelines(DOCKER_SERVER_CONFIG)
+
+        for num in range(1,sys.argv[CLIENTS_NUM_ARGP]+1):
+            client_config = [
+                f"  client{num}:\n",
+                f"    container_name: client{num}\n",
+                f"    container_name: client{num}\n",
+                 "    image: client:latest\n", 
+                 "    entrypoint: /client\n",
+                 "    environment:\n", 
+                f"      - CLI_ID={num}\n",
+                 "      - CLI_LOG_LEVEL=DEBUG\n",  
+                 "    networks:\n",
+                 "      - testing_net\n",
+                 "    depends_on:\n",
+                 "      - server\n"
+            ]
+            f.writelines(client_config)
+
+        f.writelines(DOCKER_NETWORKS_CONFIG)
+
+main()
+
+# MODELO
+""" 
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+
+  client1:
+    container_name: client1
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=1
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+
+"""
