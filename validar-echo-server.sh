ee7003e42a88b597a9fbcc8c96def5174aa27946
AlejoSendra1 <alejo.sendra12@gmail.com>
2026-03-16 21:37:02 -0300

    bash file for server echo test created

---
+++ validar-echo-server.sh
@@ -0,0 +1,18 @@
+TEST_MESSAGE="Probando ando 123"
+
+docker run -d -i --name server_echo_tester --rm --network "tp0_testing_net" ubuntu
+docker exec server_echo_tester apt update >/dev/null
+docker exec server_echo_tester apt install netcat-openbsd -y >/dev/null
+
+RESPONSE=$(echo -n "$TEST_MESSAGE" | docker exec -i server_echo_tester nc server 12345 2>/dev/null)
+
+docker stop server_echo_tester
+
+if [ "$RESPONSE" == "$TEST_MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+    exit 0
+else
+    echo "action: test_echo_server | result: fail"
+    exit 1
+fi
+
